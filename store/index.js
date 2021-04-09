07de69d925a0f97b6efb0c9bab8857b19e323ba0
Pridvorov Alexey <pridvorov@gmail.com>
2021-04-09 10:55:45 +0700

    project new index.js

---
+++ store/index.js
@@ -0,0 +1,38 @@
+export const state = () => ({
+  loading: false,
+  error: false,
+  message: null
+})
+
+export const mutations = {
+  setLoading (state, payLoad) {
+    state.loading = payLoad
+  },
+  setError (state, payLoad) {
+    state.error = true
+    state.message = payLoad
+  },
+  setMessage (state, payLoad) {
+    state.error = false
+    state.message = payLoad
+  },
+  clearMessage (state) {
+    state.error = false
+    state.message = null
+  }
+}
+
+export const actions = {}
+
+export const getters = {
+  loading (state) {
+    return state.loading
+  },
+  error (state) {
+    return state.error
+  },
+  message (state) {
+    return state.message
+  }
+
+}
