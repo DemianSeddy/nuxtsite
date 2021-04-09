010fb4f2c3f9670699fda818525f09479ea1acaa
Pridvorov Alexey <pridvorov@gmail.com>
2021-04-09 10:24:17 +0700

    project now methods

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="text-center">
+        <h1>Это сайт</h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+
+  components: {
+
+  }
+}
+</script>
