d2224a2b14e181efc4d995d54958bf30e17640cc
Pridvorov Alexey <pridvorov@gmail.com>
2021-04-09 10:29:25 +0700

    project new login.vue

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="text-center">
+        <h1>Логин</h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
